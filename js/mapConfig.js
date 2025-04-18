0c63655157a0f09185c66516ea732b166e1adb97
solanoamy <70601985+solanoamy@users.noreply.github.com>
2025-04-18 14:08:39 -0400

    increased interactivity on map by changing opacity on hover
    i also removed teh black border that would appear around the extent of the polygon. noting that we will need to change the popup to a bigger font and consider changing colors for greater contrast

--- js/mapConfig.js
+++ js/mapConfig.js
@@ -16,33 +16,67 @@ export function initializeMap(elementId, center = [38.9072, -77.0369], zoom = 15
   
     return map;
   }
-  
-  // Add neighborhood data to the map
+
+    // Add neighborhood data to the map
   export async function addNeighborhoods(map, url, style) {
     const response = await fetch(url);
     const collection = await response.json();
     
-    const data = L.geoJSON(collection, { style, onEachFeature:wardsHover,
-
-    }).addTo(map);
-    
     function wardsHover(feature, layer) {
-    // Add tooltip to neighborhoods
-      layer.bindTooltip(`${feature.properties.NAME}`);
-
       layer.on('mouseover', function() {
         layer.setStyle({
-            fillOpacity: 0.8
+            fillOpacity: 0.9,
+            weight: 2
         });
       });
       layer.on('mouseout', function() {
         layer.setStyle({
-            fillOpacity: 0.55
+            fillOpacity: 0.5,
+            weight: 0.5
           });
         });
-      };
-    
-    map.fitBounds(layer.getBounds());
+      }
 
-    return { collection, data };
+    const layer = L.geoJSON(collection, { style, onEachFeature:wardsHover }).addTo(map);
+    
+    // Add tooltip to neighborhoods
+    layer.bindTooltip(layer => {
+      const hood = layer.feature;
+      const name = hood.properties['NAME'];
+      return `${name}`;
+    });
+
+    map.fitBounds(layer.getBounds());
+    
+    return { layer, collection };
   }
+  
+  // // Add neighborhood data to the map
+  // export async function addNeighborhoods(map, url, style) {
+  //   const response = await fetch(url);
+  //   const collection = await response.json();
+    
+  //   const data = L.geoJSON(collection, { style, onEachFeature:wardsHover,
+
+  //   }).addTo(map);
+    
+  //   function wardsHover(feature, layer) {
+  //   // Add tooltip to neighborhoods
+  //     layer.bindTooltip(`${feature.properties.NAME}`);
+
+  //     layer.on('mouseover', function() {
+  //       layer.setStyle({
+  //           fillOpacity: 0.8
+  //       });
+  //     });
+  //     layer.on('mouseout', function() {
+  //       layer.setStyle({
+  //           fillOpacity: 0.55
+  //         });
+  //       });
+  //     };
+    
+  //   map.fitBounds(layer.getBounds());
+
+  //   return { collection, data };
+  // }
